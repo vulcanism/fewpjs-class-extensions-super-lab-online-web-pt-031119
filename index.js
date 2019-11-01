a62f69819b0b994eb0e6aeaaae324538135a54ca
Savannah C <sgcorrell@gmail.com>
2019-11-01 16:00:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Deciduous extends Tree {
 
   static definition() {
     return(
-      super.definition() + `Deciduous trees shed their leaves annually.`
+      super.definition() + ` Deciduous trees shed their leaves annually.`
     )
   }
 }
