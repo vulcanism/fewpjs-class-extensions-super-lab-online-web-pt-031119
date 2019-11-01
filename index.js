cb1a53b26edbaf5154598c7a7ab82a6e664b8aef
Savannah C <sgcorrell@gmail.com>
2019-11-01 15:58:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ class Tree {
   constructor(species) {
     this.species = species
   }
+
+  static definition() {
+    return `A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.`
+  }
 }
